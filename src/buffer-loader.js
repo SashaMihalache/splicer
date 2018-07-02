c98b0b9530399bf4f9ba8df57577e9c85948986b
sasha <sasha.mihalache@gmail.com>
2018-07-02 20:27:24 +0300

    refactor buffer-loader to es6

--- src/buffer-loader.js
+++ src/buffer-loader.js
@@ -1,41 +1,44 @@
-function BufferLoader(context, urlList, callback) {
-  this.context = context;
-  this.urlList = urlList;
-  this.onload = callback;
-  this.bufferList = new Array();
-  this.loadCount = 0;
-}
-
-BufferLoader.prototype.loadBuffer = function (url, index) {
-  var request = new XMLHttpRequest();
-  request.open("GET", url, true);
-  request.responseType = "arraybuffer";
-
-  var loader = this;
-
-  request.onload = function () {
-    loader.context.decodeAudioData(
-      request.response,
-      function (buffer) {
-        if (!buffer) {
-          alert('error decoding file data: ' + url);
-          return;
-        }
-        loader.bufferList[index] = buffer;
-        if (++loader.loadCount == loader.urlList.length)
-          loader.onload(loader.bufferList);
-      }
-    );
+class BufferLoader {
+  constructor(context, urlList, callback) {
+    this.context = context;
+    this.urlList = urlList;
+    this.isReady = callback;
+    this.bufferList = [];
+    this.loadCount = 0;
   }
 
-  request.onerror = function () {
-    alert('BufferLoader: XHR error');
+  loadBuffer(url, index) {
+    fetch(url)
+      .then(body => {
+        body.arrayBuffer().then(buffer => {
+          this.context.decodeAudioData(
+            buffer,
+            audio => {
+              if (!audio) alert('Error decoding file data: ' + url);
+
+              this.bufferList[index] = audio;
+
+              // when all buffers are loaded and decoded, callback
+              if (++this.loadCount == this.urlList.length) {
+                this.isReady(this.bufferList);
+              }
+            }
+          )
+        })
+          .catch(err => {
+            console.log('Error buffering', err);
+          })
+      })
+      .catch(err => {
+        alert('Buffer Loader : Fetch Error');
+      })
   }
 
-  request.send();
+  load() {
+    for (let i = 0; i < this.urlList.length; ++i) {
+      this.loadBuffer(this.urlList[i], i);
+    }
+  }
 }
 
-BufferLoader.prototype.load = function () {
-  for (var i = 0; i < this.urlList.length; ++i)
-    this.loadBuffer(this.urlList[i], i);
-}
+window.BufferLoader = BufferLoader;
